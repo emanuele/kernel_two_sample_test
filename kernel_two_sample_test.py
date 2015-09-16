3ec7e38099e06d539a2f94656b31e5a3c46ef6af
Emanuele Olivetti <olivetti@fbk.eu>
2015-09-16 17:05:22 +0200

    removed unused functions + enforced PEP8

--- kernel_two_sample_test.py
+++ kernel_two_sample_test.py
@@ -11,11 +11,12 @@ def MMD2u(K, m, n):
     Ky = K[m:, m:]
     Kxy = K[:m, m:]
     return 1.0 / (m * (m - 1.0)) * (Kx.sum() - Kx.diagonal().sum()) + \
-           1.0 / (n * (n - 1.0)) * (Ky.sum() - Ky.diagonal().sum()) - \
-           2.0 / (m * n) * Kxy.sum()
+        1.0 / (n * (n - 1.0)) * (Ky.sum() - Ky.diagonal().sum()) - \
+        2.0 / (m * n) * Kxy.sum()
 
 
-def compute_null_distribution(K, m, n, iterations=10000, verbose=False, seed=None, marker_interval=1000):
+def compute_null_distribution(K, m, n, iterations=10000, verbose=False,
+                              seed=None, marker_interval=1000):
     """Compute the bootstrap null-distribution of MMD2u.
     """
     np.random.seed(seed)
@@ -25,14 +26,17 @@ def compute_null_distribution(K, m, n, iterations=10000, verbose=False, seed=Non
             print(i),
             stdout.flush()
         idx = np.random.permutation(m+n)
-        K_i = K[idx, idx[:,None]]
+        K_i = K[idx, idx[:, None]]
         mmd2u_null[i] = MMD2u(K_i, m, n)
-        
-    if verbose: print("")
+
+    if verbose:
+        print("")
+
     return mmd2u_null
 
 
-def compute_null_distribution_given_permutations(K, m, n, permutation, iterations=10000):
+def compute_null_distribution_given_permutations(K, m, n, permutation,
+                                                 iterations=10000):
     """Compute the bootstrap null-distribution of MMD2u given
     predefined permutations.
 
@@ -41,13 +45,14 @@ def compute_null_distribution_given_permutations(K, m, n, permutation, iteration
     mmd2u_null = np.zeros(iterations)
     for i in range(iterations):
         idx = permutation[i]
-        K_i = K[idx, idx[:,None]]
+        K_i = K[idx, idx[:, None]]
         mmd2u_null[i] = MMD2u(K_i, m, n)
         
     return mmd2u_null
 
 
-def kernel_two_sample_test(X, Y, kernel_function='rbf', iterations=10000, verbose=False, **kwargs):
+def kernel_two_sample_test(X, Y, kernel_function='rbf', iterations=10000,
+                           verbose=False, **kwargs):
     """Compute MMD^2_u, its null distribution and the p-value of the
     kernel two-sample test.
 
@@ -65,31 +70,15 @@ def kernel_two_sample_test(X, Y, kernel_function='rbf', iterations=10000, verbos
     if verbose:
         print("MMD^2_u = %s" % mmd2u)
         print("Computing the null distribution.")
-    
-    mmd2u_null = compute_null_distribution(K, m, n, iterations, verbose=verbose)
-    p_value = max(1.0/iterations, (mmd2u_null > mmd2u).sum() / float(iterations))
+
+    mmd2u_null = compute_null_distribution(K, m, n, iterations,
+                                           verbose=verbose)
+    p_value = max(1.0/iterations, (mmd2u_null > mmd2u).sum() /
+                  float(iterations))
     if verbose:
         print("p-value ~= %s \t (resolution : %s)" % (p_value, 1.0/iterations))
-        
+
     return mmd2u, mmd2u_null, p_value
-
-
-def compute_upper_B_10(p):
-    """Compute a upper bound on the Bayes factor of H_1 versus H_0
-    given a p-value 'p'.
-
-    See 'Calibration of p Values for Testing Precise Null Hypotheses'
-    by Sellke et al. 2001.
-    """
-    return -1.0 / (np.e * p * np.log(p))
-
-
-def compute_lower_alpha(p):
-    """
-    See 'Calibration of p Values for Testing Precise Null Hypotheses'
-    by Sellke et al. 2001.
-    """
-    return 1.0 / (1.0 + 1.0 / ( -np.e * p * np.log(p)))
 
 
 if __name__ == '__main__':
@@ -117,17 +106,24 @@ if __name__ == '__main__':
 
     if d == 2:
         plt.figure()
-        plt.plot(X[:,0], X[:,1], 'bo')
-        plt.plot(Y[:,0], Y[:,1], 'rx')
+        plt.plot(X[:, 0], X[:, 1], 'bo')
+        plt.plot(Y[:, 0], Y[:, 1], 'rx')
 
     sigma2 = np.median(pairwise_distances(X, Y, metric='euclidean'))**2
-    mmd2u, mmd2u_null, p_value = kernel_two_sample_test(X, Y, kernel_function='rbf', gamma=1.0/sigma2, verbose=True)
-    # mmd2u, mmd2u_null, p_value = kernel_two_sample_test(X, Y, kernel_function='linear', verbose=True)
+    mmd2u, mmd2u_null, p_value = kernel_two_sample_test(X, Y,
+                                                        kernel_function='rbf',
+                                                        gamma=1.0/sigma2,
+                                                        verbose=True)
+    # mmd2u, mmd2u_null, p_value = kernel_two_sample_test(X, Y,
+    #                                                     kernel_function='linear',
+    #                                                     verbose=True)
 
     plt.figure()
     prob, bins, patches = plt.hist(mmd2u_null, bins=50, normed=True)
-    plt.plot(mmd2u, prob.max()/30, 'w*', markersize=24, markeredgecolor='k', markeredgewidth=2, label="$MMD^2_u = %s$" % mmd2u)
+    plt.plot(mmd2u, prob.max()/30, 'w*', markersize=24, markeredgecolor='k',
+             markeredgewidth=2, label="$MMD^2_u = %s$" % mmd2u)
     plt.xlabel('$MMD^2_u$')
     plt.ylabel('$p(MMD^2_u)$')
     plt.legend(numpoints=1)
-    plt.title('$MMD^2_u$: null-distribution and observed value. $p$-value=%s' % p_value)
+    plt.title('$MMD^2_u$: null-distribution and observed value. $p$-value=%s'
+              % p_value)
